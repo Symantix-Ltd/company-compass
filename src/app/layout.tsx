b3a7a22216b1307bad1ab00e26af4e4f77d69886
Tanya Gray <tanyagray@Tanyas-MacBook-Air.local>
2025-08-16 22:21:17 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
