80cf2ebfce25b36a6302be8d688477e3225b4ab3
Tanya Gray <tanyagray@Tanyas-MacBook-Air.local>
2025-09-08 11:57:42 +0100

    remove Head from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import Head from 'next/head';
 
 
 import { GoogleTagManager } from '@next/third-parties/google'
@@ -34,18 +33,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-T2HVKFJV" />
-      <Head>
-      <meta charSet="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-
+      <head>
+      
   
       <meta name="msvalidate.01" content="6F2D6D7E6622829F73D997615CEEA43D" />
       <script async src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7212919066729459"
      crossOrigin="anonymous"></script>
 
-<link rel="icon" href="/favicon.ico" />
-      </Head>
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
           <Header/>
