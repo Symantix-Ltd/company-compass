4ca30146ec4da58d600c9e012bfefbf67154d9fc
Tanya Gray <tanyagray@Tanyas-MacBook-Air.local>
2025-09-12 23:55:48 +0100

    mod ad

--- src/app/components/AdSlot.js
+++ src/app/components/AdSlot.js
@@ -3,13 +3,18 @@
 import { useEffect } from 'react'
 
 export default function AdSlot({ styleDef={display: 'block'}, client, slot, format = 'auto', responsive = 'true', layout="" }) {
-  
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
+    } catch (e) {
+      console.error('AdSense error:', e)
+    }
+  }, [])
 
   return (
 
     <div> 
-      <script async src={'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + client }
-     crossorigin="anonymous"></script>
+     
     <ins className="adsbygoogle"
       style={styleDef}
       data-ad-client={client}
@@ -24,15 +29,3 @@ export default function AdSlot({ styleDef={display: 'block'}, client, slot, form
   )
 }
 
-/*
-
-
-useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({})
-    } catch (e) {
-      console.error('AdSense error:', e)
-    }
-  }, [])
-
-  */
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,8 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7212919066729459"
           crossOrigin="anonymous"
         ></script>
+
+
       </head>
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
